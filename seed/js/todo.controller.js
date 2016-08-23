94ac9c2dd0c7a3c45a3fc5f317d115b4c5bacdfe
Álvaro Márquez <marquezalvaro13@gmail.com>
2016-08-23 12:26:00 +0200

    Lab 3

--- seed/js/todo.controller.js
+++ seed/js/todo.controller.js
@@ -3,6 +3,7 @@
   function TodoController(){
     var self = this;
 
+    self.newTodo = '';
     self.list = [{
       title:'Primera tarea',
       completed: false
@@ -12,8 +13,26 @@
     },{
       title:'Tercera tarea',
       completed: false
-    }]
+    }];
+    self.addTodo = addTodo;
+    self.removeTodo = removeTodo;
+
+    function addTodo (){
+      self.list.unshift({
+        title:self.newTodo,
+        completed:false
+      });
+      self.newTodo = '';
+    }
+
+    function removeTodo(item, index){
+      self.list.splice(index, 1);
+    }
+
+
   }
+
+
   angular
     .module('app')
     .controller('TodoController', TodoController)
