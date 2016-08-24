25840ab9f086ac51d44445fb4921d22de1f1faf4
Álvaro <marquezalvaro13@gmail.com>
2016-08-24 13:20:25 +0200

    Lab  5

--- seed/js/todo.controller.js
+++ seed/js/todo.controller.js
@@ -1,41 +1,74 @@
 (function(){
   'use strict'
-  function TodoController(){
+  function TodoController(TodoService){
     var self = this;
 
     self.newTodo = '';
-    self.list = [{
-      title:'Primera tarea',
-      completed: false
-    },{
-      title:'Segunda tarea',
-      completed: false
-    },{
-      title:'Tercera tarea',
-      completed: false
-    }];
+    self.list = [];
     self.addTodo = addTodo;
     self.removeTodo = removeTodo;
     self.getRemaining = getRemaining;
+    self.getTodos = getTodos;
+    self.updateTodo = updateTodo;
+    self.toggleState = toggleState
 
     function addTodo (){
-      self.list.unshift({
-        title:self.newTodo,
-        completed:false
-      });
-      self.newTodo = '';
+      if (!self.newTodo){
+        return;
+      }
+      TodoService
+        .create({
+          title:self.newTodo,
+          completed:false
+        })
+        .then(function(response){
+          self.list.unshift(
+            response
+          );
+          self.newTodo = '';
+        })
     }
 
     function removeTodo(item, index){
-      self.list.splice(index, 1);
+      TodoService
+        .remove(item)
+        .then(function(response){
+          self.list.splice(index, 1);
+
+        })
     }
 
     function getRemaining(){
       return self.list.filter(function (item){
-        return !item.completed
+        return !item.completed;
       })
     }
+    function getTodos(){
+      TodoService
+        .retrieve()
+        .then(function(response){
+          self.list = response;
+        });
+    }
+    function updateTodo(item, index){
+      if (!item.title) {
+        self.removeTodo(item, index);
+        return;
+      }
+      TodoService
+        .update(item);
+    }
+    function toggleState(item){
+      TodoService
+        .update(item).
+        then(function(){
 
+        }, function(){
+          item.completed = !item.completed;
+        });
+    }
+
+    self.getTodos();
   }
 
 
